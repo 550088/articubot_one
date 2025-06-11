6455ca8c3cf8c0a4c6f63863eed1c1a762852cce
550088 <alankaywthay@gmail.com>
2025-06-11 19:36:10 +0000

    created core URDF

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,41 +1,47 @@
-import os
-
-from ament_index_python.packages import get_package_share_directory
-
-from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-
+from launch import LaunchDescription
+import os
 import xacro
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
 
-    # Check if we're told to use sim time
+    # Launch configuration variables
     use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('articubot_one'))
-    xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
+    # Get URDF from xacro
+    pkg_path = get_package_share_directory('articubot_one')
+    xacro_file = os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
-    
-    # Create a robot_state_publisher node
-    params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    robot_description = {'robot_description': robot_description_config.toxml()}
+
+    # Joint State Publisher GUI node
+    node_joint_state_publisher = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    # Robot State Publisher node
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
+        name='robot_state_publisher',
         output='screen',
-        parameters=[params]
+        parameters=[robot_description, {'use_sim_time': use_sim_time}]
     )
 
-
-    # Launch!
+    # Return the LaunchDescription
     return LaunchDescription([
         DeclareLaunchArgument(
-            'use_sim_time',
+            name='use_sim_time',
             default_value='false',
-            description='Use sim time if true'),
-
-        node_robot_state_publisher
+            description='Use simulation clock if true'
+        ),
+        node_joint_state_publisher,         # önce GUI
+        node_robot_state_publisher          # sonra robot_state_publisher
     ])
